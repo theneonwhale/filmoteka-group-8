9f0547be0367c54c3b4d0dcb4896e6f3e1314ec9
Oleksii Fedorkan <alex.fedorkan@gmail.com>
2020-11-20 19:50:09 +0200

    Fix home page render.

--- src/js/fetch-home-page.js
+++ src/js/fetch-home-page.js
@@ -4,12 +4,22 @@ import 'tui-pagination/dist/tui-pagination.css';
 
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '726653b8cacb73d155407508fdc35e60';
+const genreIdsArr = [];
+fetchGenreIds();
 
+// https://api.themoviedb.org/3/movie/{movie_id}?api_key=726653b8cacb73d155407508fdc35e60&language=en-US
+// https://api.themoviedb.org/3/genre/movie/list?api_key=726653b8cacb73d155407508fdc35e60&language=en-US
 // https://api.themoviedb.org/3/movie/popular/?api_key=726653b8cacb73d155407508fdc35e60
 // https://api.themoviedb.org/3/?api_key=726653b8cacb73d155407508fdc35e60
 
 const movieListEL = document.querySelector('.js-cards-markup');
 const paginationContainer = document.getElementById('tui-pagination-container');
+
+function fetchGenreIds() {
+  fetch(`${BASE_URL}genre/movie/list?api_key=${API_KEY}`)
+    .then(responce => responce.json())
+    .then(responce => genreIdsArr.splice(0, 0, ...responce.genres));
+}
 
 function fetchDayMovies(page = 1) {
   return fetch(
@@ -28,11 +38,11 @@ fetchDayMovies().then(responce => {
   });
 
   myPagination.on('afterMove', function (eventData) {
-    let currentPage = eventData.page;
-
-    fetchDayMovies(currentPage).then(responce =>
-      renderResults(responce.results),
-    );
+    fetchDayMovies(eventData.page)
+      .then(responce => {
+        renderResults(responce.results);
+      })
+      .catch(console.log);
   });
 });
 
@@ -41,6 +51,34 @@ function renderResults(results) {
 
   results.forEach(movieObj => {
     movieObj.release_date = movieObj.release_date.slice(0, 4);
+
+    if (movieObj.genre_ids.length === 0) {
+      movieObj.genre_ids.push('No genre');
+    } else if (movieObj.genre_ids.length <= 3) {
+      movieObj.genre_ids.forEach((currentId, index) => {
+        const idObj = genreIdsArr.find(genreObj => genreObj.id === currentId);
+
+        movieObj.genre_ids[index] = `${idObj.name},`;
+      });
+
+      movieObj.genre_ids[movieObj.genre_ids.length - 1] = movieObj.genre_ids[
+        movieObj.genre_ids.length - 1
+      ].slice(0, movieObj.genre_ids[movieObj.genre_ids.length - 1].length - 1);
+    } else {
+      movieObj.genre_ids.forEach((currentId, index) => {
+        const idObj = genreIdsArr.find(genreObj => genreObj.id === currentId);
+
+        movieObj.genre_ids[index] = `${idObj.name},`;
+      });
+
+      const tempArr = [];
+
+      tempArr.push(movieObj.genre_ids[0]);
+      tempArr.push(movieObj.genre_ids[1]);
+      tempArr.push('Other');
+
+      movieObj.genre_ids.splice(0, movieObj.genre_ids.length, ...tempArr);
+    }
   });
 
   movieListEL.insertAdjacentHTML('afterbegin', movieItemsTpl(results));
